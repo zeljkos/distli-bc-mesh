32f929b83ec597b68422bd118ddf49dcb2658191
zeljkos <zeljkos@gmx.ch>
2025-06-15 23:21:40 +0200

    docker-compose added - nginx server in fron of the 3 validator in the enterprise bc

--- extracted_files/src:common:mod.rs
+++
@@ -1,7 +0,0 @@
-// Common shared modules between tracker and enterprise blockchain
-//
-pub mod blockchain;
-pub mod types; 
-pub mod crypto;
-pub mod time;
-pub mod api_utils;
--- extracted_files/src:tracker:mod.rs
+++
@@ -1,8 +0,0 @@
-// Tracker module - WebRTC peer discovery and multi-tenant networks
-pub mod server;
-pub mod integration;
-
-// Re-export main types with proper names
-pub use server::Tracker;
-pub use integration::EnterpriseIntegration;
-pub use integration::BlockchainUpdate;
